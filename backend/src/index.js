c6b85662d811a0aa7d024b0fc9191a718236954a
Gaura89744 <gauravsrivastavass007@gmail.com>
2025-08-16 13:03:01 +0530

    Project Completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,61 +1,29 @@
-// import express from "express";
-// import dotenv from "dotenv";
-// import authRoutes from "./routes/auth.route.js";
-// import { connectDB } from "./lib/db.js";
-// import cookieparser from "cookie-parser";
-// import messageRoutes from "./routes/message.route.js";
-// import cors from "cors";
-// import { app, server } from "./lib/socket.js";
-// import path from "path";
-// dotenv.config();
-
-// app.use(express.json());
-// app.use(cookieparser());
-// app.use(
-//   cors({
-//     origin: "http://localhost:5173",
-//     credentials: true,
-//   })
-// );
-// const PORT = process.env.PORT;
-// const __dirname = path.resolve();
-// app.use("/api/auth", authRoutes);
-// app.use("/api/messages", messageRoutes);
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "../frontend/dist")));
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-//   });
-// }
-
-// server.listen(PORT, () => {
-//   console.log("Server is listening at port" + PORT);
-
-//   connectDB();
-// });
 import express from "express";
 import dotenv from "dotenv";
-import authRoutes from "./routes/auth.route.js";
-import { connectDB } from "./lib/db.js";
-import cookieparser from "cookie-parser";
-import messageRoutes from "./routes/message.route.js";
+import cookieParser from "cookie-parser";
 import cors from "cors";
-import { app, server } from "./lib/socket.js";
+
 import path from "path";
+
+import { connectDB } from "./lib/db.js";
+
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
 
 dotenv.config();
 
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
+
 app.use(express.json());
-app.use(cookieparser());
+app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: "http://localhost:5173",
     credentials: true,
   })
 );
-
-const PORT = process.env.PORT || 5000;
-const __dirname = path.resolve();
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
@@ -63,13 +31,12 @@ app.use("/api/messages", messageRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("/*", (req, res) => {
+  app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
 
 server.listen(PORT, () => {
-  console.log("🚀 Server is listening at port " + PORT);
+  console.log("server is running on PORT:" + PORT);
   connectDB();
 });
-
--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -1,12 +1,10 @@
-import mongoose from "mongoose"
-export const connectDB= async () => {
-    try {
-        const conn = await mongoose.connect(process.env.MONGODB_URI);
-        console.log(`Mongodb connected: ${conn.connection.host}`)
-        console.log("  DB Name:", conn.connection.name);
-       
+import mongoose from "mongoose";
 
-    } catch (error) {
-        console.log("Mongodb connection error:",error)
-    }
+export const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    console.log(`MongoDB connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log("MongoDB connection error:", error);
+  }
 };

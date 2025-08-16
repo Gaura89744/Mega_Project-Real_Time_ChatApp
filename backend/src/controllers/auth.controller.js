04ef4b276acc55dbdc4c353771dd55d07812b9c7
Gaura89744 <gauravsrivastavass007@gmail.com>
2025-08-16 12:21:06 +0530

    project updated

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -3,45 +3,6 @@ import User from "../models/user.model.js"
 import bcrypt from "bcryptjs"
 import cloudinary from '../lib/cloudinary.js'; // adjust the path if different
 
-// export const signup=async(req,res)=>{
-//    const {email,fullName,password}=req.body;
-//    try {
-//         if(password.length < 6){
-//             return res.status(400).json({message:"Password must be atleast 6 character"})
-//         }
-
-//         const user =await User.findOne({email});
-
-//         if(user) return res.status(400).json({message:"Email already exist"})
-        
-//             const salt = await bcrypt.genSalt(10);
-//                 const hashedpassword = await bcrypt.hash(password,salt)
-        
-//                 const newUser = new User({
-//                     email,
-//                     fullName,
-//                     password:hashedpassword
-//                 })
-
-//                 if (newUser) {
-//                     //generate jwt token here
-//                   generateToken(newUser._id,res)
-//                   await newUser.save()
-
-//                   res.status(201).json({
-//                     _id:newUser._id,
-//                     fullName:newUser.fullName,
-//                     email:newUser.email,
-//                     profilePic:newUser.profilePic,
-//                   })
-//                 } else {
-//                     return res.status(400).json({message:"Invalid user data"})
-//                 }
-//     } catch (error) {
-//         console.log("Error in signup controller",error.message)
-//         res.status(500).json({message:"Internal Server Error"})
-//     }
-// }
 export const signup = async (req, res) => {
   const { email, fullName, password } = req.body;
 
